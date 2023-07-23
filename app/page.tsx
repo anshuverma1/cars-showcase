7ea1c3d0c27d13a5785d313e75c424faabbe32f4
Anshu Verma <verma.anshu96@gmail.com>
2023-07-23 15:22:38 +0530

    created modal

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,53 @@
+import CarCard from '@/components/CarCard'
+import CustomFilter from '@/components/CustomFilter'
 import Hero from '@/components/Hero'
-import Image from 'next/image'
+import SearchBar from '@/components/SearchBar'
+import { CarProps } from '@/types'
+import { fetchCars } from '@/utils'
 
-export default function Home() {
+export default async function Home() {
+
+  const allCars = await fetchCars()
   return (
     <main className="overflow-hidden">
       <Hero />
+
+      <div className='mt-12 padding-x padding-y max-width' id='discover'>
+
+        <div className="home__text-container">
+          <h1 className='text-4xl font-extrabold'>
+            Car Catalogue
+          </h1>
+          <p>Explore the cars you might like</p>
+        </div>
+
+        <div className='home__filters'>
+          <SearchBar />
+
+          <div className='home__filter-container'>
+            <CustomFilter title='fuel' />
+            <CustomFilter title='year' />
+          </div>
+        </div>
+
+        {allCars.length > 0 ?
+          <section>
+            <div className='home__cars-wrapper'>
+              {allCars.map((car: CarProps, index: React.Key) => {
+                return <CarCard car={car} key={index} />
+              })}
+            </div>
+          </section>
+          :
+          (
+            <div className='home__error-container'>
+              <h2 className='text-black text-xl font-bold'>No results</h2>
+              <p>{allCars?.message}</p>
+            </div>
+          )
+        }
+
+      </div>
     </main>
   )
 }
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,19 +1,25 @@
-'use client'
+'use client';
 
 import { ButtonProps } from '@/types'
+import Image from 'next/image'
 import React from 'react'
 
-const Button = ({ title, containerStyle, handleClick }: ButtonProps) => {
+const Button = ({ title, containerStyle, handleClick, btnType, textStyles, rightIcon }: ButtonProps) => {
   return (
     <button
       disabled={false}
-      type={'button'}
+      type={btnType || 'button'}
       className={`custom-btn ${containerStyle}`}
       onClick={handleClick}
     >
-      <span className={`flex-1`}>
+      <span className={`flex-1 ${textStyles}`}>
         {title}
       </span>
+      {
+        rightIcon && <div className='relative w-6 h-6'>
+          <Image src={rightIcon} fill alt='right arrow' />
+        </div>
+      }
     </button>
   )
 }
